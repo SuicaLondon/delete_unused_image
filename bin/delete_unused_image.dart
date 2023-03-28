c8be985217c4df3e7841ab4d1d501ed42d0fc33c
SuicaDavid <suicadevinlondon@gmail.com>
2023-03-29 00:16:05 +0100

    init project

---
+++ bin/delete_unused_image.dart
@@ -0,0 +1,5 @@
+import 'package:delete_unused_image/delete_unused_image.dart' as delete_unused_image;
+
+void main(List<String> arguments) {
+  print('Hello world: ${delete_unused_image.calculate()}!');
+}
---
+++ test/delete_unused_image_test.dart
@@ -0,0 +1,8 @@
+import 'package:delete_unused_image/delete_unused_image.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
